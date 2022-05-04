1db9870de2508a13b50a47134fb0c8bc74beb421
Turner Milan <turnermilan@gmail.com>
2022-05-04 12:12:16 -0400

    added a couple API routes

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -42,3 +42,56 @@ export const login = async (username, password) => {
      console.error(error)
  }
  } 
+
+
+// GET USER 
+export const getUser = async () => {
+  try { 
+   const response = await fetch(`${url}/users/me`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem("token")}`
+      },
+    })
+    const data =  await response.json()
+    return data;
+  
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// ALL ACTIVITIES
+export const getAllActivities = async () => {
+  try{
+    const response = await fetch(`${url}/activities`, {
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    const allActivities = await response.json()
+    console.log(allActivities)
+    return allActivities
+  }catch(error){
+    console.error(error) 
+  }
+}
+
+// CREATE NEW ACTIVITY 
+export const creatActivity = async (name, description) => {
+  try{
+    const response = await fetch(`${url}/activities`, {
+      method: 'POST', 
+      body: JSON.stringify({
+        name: name,
+        description: description
+      })
+    })
+    const newActivity = await response.json()
+    console.log(newActivity)
+    return newActivity
+  }catch(error){
+    console.error(error)
+  }
+}
+
